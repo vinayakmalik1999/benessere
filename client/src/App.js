c38f4e94fd2c33364c1846e66ad2662f793dfc0f
vinayakmalik1999 <vinayakmalik1999@gmail.com>
2020-06-10 12:28:23 -0700

    Created form UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import axios from 'axios'
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+       formValue:''
+     }
+     this.handleChange = this.handleChange.bind(this)
+     this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+  handleChange(event){
+   this.setState({formValue:event.target.value})
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    if(this.state.formValue === ''){
+      alert("please enter a value")
+      return;
+    }
+    axios.post("http://localhost:3001/api/putData",{
+      URL:this.state.formValue
+    }).then(res => {
+      console.log(res)
+      this.setState({formValue:''})
+    })
+    .catch(err => {
+      alert(err)
+      return;
+    })
+
+}
+
+
+  render(){
+    return (
+      <div className="App">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Shorten URL's
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <header className="App-header">
+        <form onSubmit = {this.handleSubmit}>
+        <InputGroup>
+        <InputGroup.Prepend>
+        <InputGroup.Text>Enter your URL</InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl as="textarea" aria-label="With textarea" value={this.state.formValue} onChange={this.handleChange}/>
+        <InputGroup.Append>
+       <Button type='submit' variant="outline-dark" >Submit</Button>
+   </InputGroup.Append>
+        </InputGroup>
+
+        </form>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
